ffae73d2748a508b5493626c2e0881de59ae130b
algaruda <algarud@gmail.com>
2016-11-26 17:54:44 +0200

    Working simple RTU bot

--- SkypeWise/SkypeWise/Controllers/MessagesController.cs
+++ SkypeWise/SkypeWise/Controllers/MessagesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,13 +10,14 @@ using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 using SkypeWise.Helpers;
+using HtmlAgilityPack;
 
 namespace SkypeWise
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
-        string resultPage="";
+        string resultPage = string.Empty;
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -37,12 +39,14 @@ namespace SkypeWise
                 }
                 else if (activity.Text.ToLower().StartsWith("research "))
                 {
+                    var searchTerm = activity.Text.Remove(0, "research ".Count());
+
                     var reply = activity.CreateReply("I'll be back with result...");
                     await connector.Conversations.ReplyToActivityAsync(reply);
 
                     reply = activity.CreateReply("Wait...");
 
-                    GetResearch("cryptography");
+                    GetResearch(searchTerm);
                     while (resultPage == "")
                     {
                         System.Threading.Thread.Sleep(1000);
@@ -70,11 +74,51 @@ namespace SkypeWise
         public async void GetResearch(string topic)
         {
             HttpClient client = new HttpClient();
-            string result = await client.GetStringAsync("https://ortus.rtu.lv/science/lv/publications/search/" + topic);
-            int start = result.IndexOf("resultTable");
-            int stop = result.Substring(start).IndexOf("</tbody></table>");
+            var baseUrl = "https://ortus.rtu.lv";
+            var requestUrl = baseUrl + "/science/lv/publications/search/" + topic;
+            var result = await client.GetStringAsync(requestUrl);
 
-            resultPage = result.Substring(start, stop);
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.OptionFixNestedTags = true;
+            htmlDoc.LoadHtml(result);
+
+            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0 && htmlDoc.DocumentNode == null)
+            {
+                resultPage = "Oops, couldn't find anything";
+                return;
+            }
+
+            var resultsToReturn = string.Empty;
+            var resultsTable = htmlDoc.GetElementbyId("resultTable");
+            var resultsTableBody = htmlDoc.DocumentNode.SelectSingleNode("/html[1]/body[1]/div[1]/div[2]/table[1]/tbody[1]");
+
+            if (resultsTableBody == null)
+            {
+                resultPage = "Oops, nothing found";
+                return;
+            }
+
+            var childs = resultsTableBody.ChildNodes.Where(o => !string.IsNullOrEmpty(o.InnerHtml) && o.InnerHtml != "\n");
+
+            var childsToServe = childs.Take(5).ToList();
+
+            var index = 0;
+            foreach (var node in childsToServe)
+            {
+                index++;
+                var inspectingNode = node.SelectSingleNode("td[2]/a[1]");
+                var caption = inspectingNode.InnerText;
+                var link = inspectingNode.GetAttributeValue("href", "/");
+
+                resultsToReturn += index.ToString() + ". " + caption + " : " + baseUrl + link + "\n\n";
+            }
+
+            if(childs.Count() > 5)
+            {
+                resultsToReturn += "\n\nAnd there is more: " + requestUrl;
+            }
+
+            resultPage = resultsToReturn;
         }
 
         private Activity HandleSystemMessage(Activity message)
