d20e6f836d4e4e99a6654f7f62ab311b27189f1e
ArtursO <ArtursO@NB083>
2016-11-26 14:32:37 +0200

    Added functionality to research topics

--- SkypeWise/SkypeWise/Controllers/MessagesController.cs
+++ SkypeWise/SkypeWise/Controllers/MessagesController.cs
@@ -8,12 +8,14 @@ using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
+using SkypeWise.Helpers;
 
 namespace SkypeWise
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        string resultPage="";
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -33,6 +35,23 @@ namespace SkypeWise
                     var replyWithConfirmation = activity.CreateReply($"You try to transfer {regMatch.Groups[0]} EUR monies to someone");
                     await connector.Conversations.ReplyToActivityAsync(replyWithConfirmation);
                 }
+                else if (activity.Text.ToLower().StartsWith("research "))
+                {
+                    var reply = activity.CreateReply("I'll be back with result...");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+
+                    reply = activity.CreateReply("Wait...");
+
+                    GetResearch("cryptography");
+                    while (resultPage == "")
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        await connector.Conversations.ReplyToActivityAsync(reply);
+                    };
+
+                    reply = activity.CreateReply(resultPage);
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
                 else
                 {
                     //// return our reply to the user
@@ -46,6 +65,16 @@ namespace SkypeWise
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        public async void GetResearch(string topic)
+        {
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync("https://ortus.rtu.lv/science/lv/publications/search/" + topic);
+            int start = result.IndexOf("resultTable");
+            int stop = result.Substring(start).IndexOf("</tbody></table>");
+
+            resultPage = result.Substring(start, stop);
         }
 
         private Activity HandleSystemMessage(Activity message)
---
+++ SkypeWise/SkypeWise/Helpers/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Connector;
+
+namespace SkypeWise.Helpers
+{
+    public static class StringHelper
+    {
+        public static Activity toReply(this string message)
+        {
+            return new Activity().CreateReply(message);
+        }
+    }
+}
